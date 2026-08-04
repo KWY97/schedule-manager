dfc8ff177aa83f16b8ecdf2767930be69e201f65
kwy97 <kwy1547@gmail.com>
2026-08-04 17:40:03 +0900

    feat: ScheduleSpot Repository 추가

---
+++ src/main/java/com/example/manage/repository/ScheduleSpotRepository.java
@@ -0,0 +1,7 @@
+package com.example.manage.repository;
+
+import com.example.manage.domain.ScheduleSpot;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScheduleSpotRepository extends JpaRepository<ScheduleSpot, Long> {
+}
