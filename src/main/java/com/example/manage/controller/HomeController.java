7df179832157f3938ec586d89e85a080631d33fc
kwy97 <kwy1547@gmail.com>
2026-09-03 16:33:16 +0900

    feat: HS 지도 표시 및 패널 연동

--- src/main/java/com/example/manage/controller/HomeController.java
+++ src/main/java/com/example/manage/controller/HomeController.java
@@ -1,13 +1,24 @@
 package com.example.manage.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Value("${kakao.maps.javascript-key}")
+    private String kakaoMapsJavaScriptKey;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+
+        model.addAttribute(
+                "kakaoMapsJavaScriptKey",
+                kakaoMapsJavaScriptKey
+        );
+
         return "home";
     }
 }
