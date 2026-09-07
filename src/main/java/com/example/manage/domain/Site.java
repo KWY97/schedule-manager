dd4cfb5de2cdea51e8a411c87cf841037c95ae47
kwy97 <kwy1547@gmail.com>
2026-09-07 20:24:11 +0900

    feat: site 엔터티 및 Repository 추가

---
+++ src/main/java/com/example/manage/domain/Site.java
@@ -0,0 +1,38 @@
+package com.example.manage.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Site {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long siteId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private Double latitude;
+
+    @Column(nullable = false)
+    private Double longitude;
+
+    @Column(nullable = false)
+    private Integer mapLevel;
+
+    public Site(String name, String address, Double latitude, Double longitude, Integer mapLevel) {
+        this.name = name;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.mapLevel = mapLevel;
+    }
+}
---
+++ src/main/java/com/example/manage/repository/SiteRepository.java
@@ -0,0 +1,8 @@
+package com.example.manage.repository;
+
+import com.example.manage.domain.Site;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SiteRepository extends JpaRepository<Site, Long> {
+
+}
